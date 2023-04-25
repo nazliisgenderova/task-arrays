d12d9dc86a6e6dfa2de4856944439df495260453
Nurlan622 <7bxwn9m@code.edu.az>
2023-04-25 17:29:16 +0400

    task-arrays

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// let obj = {
+//     arr : [2,6,10,18],
+//     add: function (num) {
+//         return this.arr[this.arr.length++]=num
+//     }
+// }
+
+// obj.add(4);
+// console.log(obj.arr);
+
+var arr=[1,2,3,4];
+arr.pop();
+console.log(arr);
